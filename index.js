1e1d6e7303844ffb613535d5ebe3915237636f9b
Kool Dmitry <njkita007@yandex.ru>
2023-05-26 13:05:40 +0300

    authorization working

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const authRouter = require('./authRouter')
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter)
 const start = async () => {

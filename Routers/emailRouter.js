e7a6c414e78e5b95553ae6be6c97a02baf363cbf
Nikita <njkita007@yandex.ru>
2023-08-02 14:20:52 +0300

    rename path const emailController = require('../controllers/emailController');fdf

--- Routers/emailRouter.js
+++ Routers/emailRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const emailController = require('../controllers/emailController');
+const emailController = require('../Controllers/emailController');
 
 // POST /emails - создание новой записи
 router.post('/create-email', emailController.createEmail);
